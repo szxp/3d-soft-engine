3a39a0d6c352f0ddccbd7705a6a1e6b6b7343ffc
peti <szp0530@gmail.com>
2014-03-10 01:29:03 +0100

    Frame limiting added

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -1,22 +1,35 @@
 
 #include "Screen.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
 
-using namespace g3;
+g3::Screen::Screen():
+frontBuffer {Gdk::Pixbuf::create(Gdk::Colorspace::COLORSPACE_RGB, true, 8, 640, 480)},
+backBuffer {Gdk::Pixbuf::create(Gdk::Colorspace::COLORSPACE_RGB, true, 8, 640, 480)},
+targetFrameTime {2000000}, // micro
+lastFrameCompleted {Clock::now()}
+{
 
-Screen::Screen():
-frontBuffer(Gdk::Pixbuf::create(Gdk::Colorspace::COLORSPACE_RGB, true, 8, 640, 480)),
-backBuffer(Gdk::Pixbuf::create(Gdk::Colorspace::COLORSPACE_RGB, true, 8, 640, 480)) {
+	clear();
 
-	// Fill the buffer with color white
-	frontBuffer->fill(0xffffffff);
-
-	// Fill the buffer with color white
-	backBuffer->fill(0xffffffff);
-
+	// register idle function
+	Glib::signal_idle().connect(sigc::mem_fun(*this, &Screen::on_idle));
 }
 
 
-bool Screen::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
+void g3::Screen::clear()
+{
+	// Fill the buffer with color white
+	frontBuffer->fill(0xffffffff);
+	
+	// Fill the buffer with color white
+	backBuffer->fill(0xffffffff);
+}
+
+bool g3::Screen::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
+	
+	clear();
 
 	Gtk::Allocation allocation = get_allocation();
 	const int width  = allocation.get_width();
@@ -28,3 +41,30 @@ bool Screen::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
 
 	return true;
 }
+
+
+/**
+ * This idle callback function is executed as often as possible, 
+ * hence it is ideal for processing intensive tasks.
+ */
+bool g3::Screen::on_idle()
+{
+	TimePoint now = Clock::now();
+	auto elapsed = std::chrono::duration_cast<MicroSeconds>(now - lastFrameCompleted);
+	std::cout << "Elapsed: " << elapsed.count() << std::endl;
+	lastFrameCompleted = now;
+
+	// sleep
+	MicroSeconds wait {(elapsed <= targetFrameTime)
+		? (targetFrameTime - elapsed) + targetFrameTime
+		: targetFrameTime - (elapsed % targetFrameTime)};
+
+	std::cout << "Sleep for: " << wait.count() << std::endl;
+	std::this_thread::sleep_for( wait );
+
+	queue_draw();
+
+	return true;
+}
+
+
--- src/Screen.h
+++ src/Screen.h
@@ -3,9 +3,15 @@
 #define SCREEN_H
 
 #include <gtkmm.h>
+#include <chrono>
 
 namespace g3
 {
+
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::time_point<Clock> TimePoint;
+typedef std::chrono::microseconds MicroSeconds;
+
 
 /**
  * Represents a 2D screen. It uses double buffering.
@@ -16,12 +22,34 @@ class Screen: public Gtk::DrawingArea {
 	Screen();
 	virtual ~Screen(){};
 	
+	/**
+	 * Clears the buffer.
+	 */
+	void clear();
+
+	/**
+	 * This idle callback function is executed as often as possible, 
+	 * hence it is ideal for processing intensive tasks.
+	 */
+	bool on_idle();
+
 	virtual bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr);
 
 
 	private:
 	Glib::RefPtr<Gdk::Pixbuf> frontBuffer;
 	Glib::RefPtr<Gdk::Pixbuf> backBuffer;
+
+	/**
+	 * The target frame time in microseconds.
+	 */
+	MicroSeconds targetFrameTime;
+
+	/**
+	 * The time point when the last frame is completed.
+	 */
+	TimePoint lastFrameCompleted;
+
 };
 
 }
